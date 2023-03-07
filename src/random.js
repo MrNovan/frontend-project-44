b66ad22a7464e6c0f84c2d0554ed4b29b5c66a3b
MrNovan <toni.justdoit@bk.ru>
2023-03-07 09:45:43 +0300

    fix code

--- src/random.js
+++ src/random.js
@@ -1,4 +1,4 @@
-const getRandomNumber = (max = 0, min = 99) => {
+const getRandomNumber = (min = 0, max = 20) => {
   const result = Math.floor(Math.random() * (max - min + 1) + min);
   return result;
 };
