3c46282b5b95e5455fe778d70e852d5dea31457a
MrNovan <toni.justdoit@bk.ru>
2023-03-13 15:13:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const playGame = (getRoundData, gameSay) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello!, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 
   console.log(gameSay);
   const Rounds = 3;
